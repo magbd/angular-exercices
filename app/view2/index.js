06ebefe41052dbf21eaec5c80a86f80470cec2f4
Magali <magali.baude@hawk-platform.io>
2018-06-08 10:06:28 +0200

    comics list with mock

---
+++ app/view2/index.js
@@ -0,0 +1,3 @@
+// import './view2.module';
+// import './view2.states';
+// // import './view2.service';
---
+++ app/view3/view3.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('myApp.view3', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view3', {
+    templateUrl: 'view3/view3.html',
+    controller: 'View3Ctrl'
+  })
+}])
+
+.controller('View3Ctrl' , ['$scope', '$http', function($scope, $http) {
+  $scope.peoples = []
+  $http({
+    method: 'GET',
+    url: 'https://swapi.co/api/people/?page=1'
+  }).then(function successCallback(response) {
+      $scope.peoples = response.data
+      console.log('response', response.data)
+      console.log('peoples', $scope.peoples)
+    }, function errorCallback(response) {
+      console.log(response.error)
+    });
+    
+  }
+])
